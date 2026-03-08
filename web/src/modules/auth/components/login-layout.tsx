50a49be2c0e40eb31942bd0381962e7db95cdf45
kotlovyim-dev <maksym.kotlovyi.dev@gmail.com>
2026-03-08 12:35:58 +0200

    feat: Implement guest guard for authentication pages and add dedicated database and cache services to the development environment.

---
+++ web/src/modules/auth/components/login-layout.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image"
+import { GuestGuard } from "@/modules/auth/components/guest-guard"
+
+export const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <GuestGuard>
+            <div className="h-screen relative flex items-center justify-center bg-secondary p-4 lg:p-8">
+                <div className="flex w-full max-w-7xl h-auto min-h-[770px] bg-card rounded-3xl shadow-xl overflow-hidden flex-col md:flex-row">
+
+                    <div className="hidden md:flex flex-col flex-1 max-w-2xl bg-primary p-10 lg:p-16 relative overflow-hidden">
+
+                        <div className="flex items-center gap-3 mb-16 z-10">
+                            <div className="flex bg-card rounded-xl p-2 w-12 h-12 justify-center items-center">
+                                <Image src="/logo.svg" alt="Woorkroom Logo" width={32} height={32} />
+                            </div>
+                            <span className="text-primary-foreground text-3xl font-bold tracking-tight">Woorkroom</span>
+                        </div>
+
+                        <div className="z-10 mt-4">
+                            <h1 className="text-primary-foreground text-4xl leading-snug font-bold">
+                                Your place to work<br />
+                                <span className="opacity-90">Plan. Create. Control.</span>
+                            </h1>
+                        </div>
+
+                        <div className="mt-10 flex flex-1 items-center justify-center w-full h-full pb-4 opacity-95 pointer-events-none">
+                            <div className="relative w-full max-w-lg h-96">
+                                <Image
+                                    src="/auth-Illustration.svg"
+                                    alt="Auth Illustration"
+                                    fill
+                                    className="object-contain"
+                                    priority
+                                />
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="flex-1 flex flex-col justify-center items-center p-8 md:p-16 lg:p-24 relative overflow-y-auto">
+                        <div className="w-full max-w-md mx-auto">
+                            {children}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </GuestGuard>
+    )
+}
